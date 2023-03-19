05d68ade73a59f4f909bde574078f7f22fe6bab3
Fabian Hillary <fabosome@gmail.com>
2023-03-19 08:02:39 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *Description:main-prints out numbers of base sixteen
+ *Return:Always successful
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 16 ; i++)
+	for (i = 0; i < 16 ; i++)
+	{
+		if (i < 10)
+		{
+		putchar(i + '0');
+		}
+		else
+		{
+		putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

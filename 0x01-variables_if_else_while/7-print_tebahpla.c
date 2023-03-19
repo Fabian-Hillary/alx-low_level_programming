c39f0cca38a6fe7ad117330a73e332f2484d16b7
Fabian Hillary <fabosome@gmail.com>
2023-03-19 07:44:31 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *Description:main-prints out alphabeautical letters in reverse
+ *Return:Always successfull
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

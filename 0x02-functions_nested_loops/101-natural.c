fdc17077bf90a2fa813550183705ac162b6d578f
Fabian Hillary <fabosome@gmail.com>
2023-03-23 06:29:48 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_multiples_of_3_or_5 :-sums up multiples of 3 and 5 from 0 to 1024
+ *Return:void
+ */
+void print_multiples_of_3_or_5(void)
+{
+	int i;
+
+	int sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+	if ((i % 3) == 0 || (i % 5) == 0)
+	{
+	sum = sum + i;
+	}
+	}
+	printf("%d\n", sum);
+}
+/**
+ * main-entry point
+ * Return:- 0 on success
+ */
+int main(void)
+{
+	print_multiples_of_3_or_5();
+	return (0);
+}

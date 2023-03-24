bdf0f6dc3854c96dcfe112fa4a103b129e2759a8
Fabian Hillary <fabosome@gmail.com>
2023-03-24 03:00:35 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,31 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_numbers - prints out numbers frim 0-9
+ * Return:void
  */
-int _putchar(char c)
-{
-	return (putchar(c));
-}
+int _putchar(char c);
 
-/**
- * print_numbers - prints out numbers from 0-9
- * Return: void
- */
 void print_numbers(void)
 {
 	int i = 0;
 
 	while (i < 10)
 	{
-		_putchar(i + '0');
+		_putchar(i + 48);
 		i++;
 	}
 	_putchar('\n');
 }
-

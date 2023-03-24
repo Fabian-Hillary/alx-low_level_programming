0f54b121020d48e7f834947363864104ab3f1025
Fabian Hillary <fabosome@gmail.com>
2023-03-24 03:58:06 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - prints numbers from 0 -14
+ * Return:void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	int j;
+	
+	for (j = 0; j <= 14; j++)
+	{
+	if (j >= 10)
+	{
+	putchar((j / 10) + 48);
+	}
+	putchar((j % 10) + 48);
+	}
+	putchar('\n');
+	i++;
+	}
+}

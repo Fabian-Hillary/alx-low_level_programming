5c69e04c580321ba575cc0be2522953f562355b0
Fabian Hillary <fabosome@gmail.com>
2023-03-30 22:42:54 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *rot13-encodes string using r13
+ * @s:string to be encoded
+ * Return:char -encoded string
+ */
+char *rot13(char *s)
+{
+	int i = 0, j;
+	char old[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char new[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*(s + i) != '\0')
+	{
+	for (j = 0; j < 52; j++)
+	{
+	if (*(s + i) == old[j])
+	{
+	*(s + i) = new[j];
+	break;
+	}
+	}
+	i++;
+	}
+	return (s);
+}

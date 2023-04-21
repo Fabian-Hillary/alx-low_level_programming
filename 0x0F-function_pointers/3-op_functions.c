355cd278ddfb5e86f9fb0834cd10c417779177ca
Fabian Hillary <fabosome@gmail.com>
2023-04-20 20:58:47 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add-sums two numbers
+ * @a:first number
+ * @b:second number
+ * Return:the sum of the two nums
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub-gets difference between two numbers
+ * @a:first number
+ * @b:second number
+ * Return:the diff of the two nums
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul-multiplies two numbers
+ * @a:first number
+ * @b:second number
+ * Return:the multiplication of the two nums
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div-divides two numbers
+ * @a:first number
+ * @b:second number
+ * Return:the division of the two nums
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	return (100);
+	exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod-two numbers
+ * @a:first number
+ * @b:second number
+ * Return:the ramainder after division of the two nums
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+	printf("Error\n");
+	return (100);
+	exit(100);
+	}
+	return (a % b);
+}
